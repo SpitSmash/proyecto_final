d2fbde8ad90aebe4b18c0c48c5133b849b80fa3c
AngelClase <angelbenitezhernandez@alumno.ieselrincon.es>
2022-03-08 14:15:17 +0000

    Controladores, Modelos, Migrates y Seeders Añadidos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // $this -> call(UserSeeder::class);
+        $this -> call(ShipSeeder::class);
+        $this -> call(BaySeeder::class);
+        $this -> call(PenaltySeeder::class);
     }
 }
